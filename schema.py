1b71453fe1886eb6bf34809581509c6e2ec693a0
1LCB <lucascb777@hotmail.com>
2025-05-19 21:46:54 -0300

    refact: changing the field descriptions

--- schema.py
+++ schema.py
@@ -10,8 +10,8 @@ class FirstStep(BaseModel):
     final_answer: str = Field("", description="this field should only be used if the answer is obvious or it has no need for a function call")
 
 class NextStep(BaseModel):
-    thought: str = Field(..., description="your reasoning about the result")
-    execute_function: bool = Field(..., description="if necessary to execute a function")
-    function_name: str = Field(..., description="the name of one of the available functions")
-    function_parameters: str = Field(..., description="json string containing the parameters for the function")
-    final_answer: str = Field("", description="your complete answer to the question if the task is finished")
+    thought: str = Field(..., description="Your reasoning about the last result")
+    execute_function: bool = Field(..., description="Whether to call another function")
+    function_name: str = Field(..., description="Name of the next function to execute")
+    function_parameters: str = Field(..., description="Parameters for the function as a json string, if any")
+    final_answer: str = Field(..., description="Your complete answer, once all steps are done")
